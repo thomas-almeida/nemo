8b7c769f90aec24a10788b8acdf5830b4a778a45
thomas almeida <contato.thomasalmeidard@gmail.com>
2025-11-15 16:35:31 -0300

    SignIn & SignUp in the same endpoint

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,14 +3,25 @@ import User from "../models/User.js"
 export const createUser = async (req, res) => {
     try {
         const { username, email } = req.body;
+
         if (!username || !email) {
             return res.status(400).json({
                 success: false,
                 error: "Nome e email são obrigatórios"
             });
         }
+
+        const userExist = await User.findOne({ email });
+
+        if (userExist) {
+            return res.status(200).json({
+                success: true,
+                user: userExist._id,
+            });
+        }
+
         const user = await User.create({ username, email });
-        res.json({
+        res.status(201).json({
             success: true,
             user,
             message: "Usuário criado com sucesso"
