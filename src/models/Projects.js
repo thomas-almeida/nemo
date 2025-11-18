b55057c342d7d1f78e42e04b24e9ebee3ff6c324
thomas almeida <contato.thomasalmeidard@gmail.com>
2025-11-18 00:13:53 -0300

    Project Controller

---
+++ src/models/Projects.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose";
+
+
+const BuildingUnit = {
+    footage: Number,
+    price: Number,
+    status: String,
+    details: String,
+}
+
+const LocationItem = {
+    name: String,
+    distance: Number,
+}
+
+const AttachmentItem = {
+    name: String,
+    url: String,
+    type: String,
+}
+
+const CopyMessage = {
+    title: String,
+    message: String,
+    createdAt: { type: Date, default: Date.now },
+}
+
+const Owner = {
+    id: String,
+    role: String,
+}
+
+const ProjectSchema = new mongoose.Schema({
+    info: {
+        name: { type: String, required: true },
+        address: { type: String, required: true },
+        developer: { type: String, required: true },
+        company: { type: String, required: true },
+        launchDate: { type: Date, default: Date.now },
+        releaseDate: { type: Date, default: Date.now },
+        details: { type: String, default: "" }
+    },
+    owner: {
+        type: [Owner],
+        required: true,
+    },
+    units: {
+        type: [BuildingUnit],
+        required: false,
+        default: []
+    },
+    location: {
+        type: [LocationItem],
+        required: false,
+        default: []
+    },
+    attachments: {
+        type: [AttachmentItem],
+        required: false,
+        default: []
+    },
+    copyMessages: {
+        type: [CopyMessage],
+        required: false,
+        default: []
+    },
+    createdAt: { type: Date, default: Date.now },
+    customersLists: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "CustomerList",
+            required: false,
+            default: null
+        }
+    ]
+});
+
+const Project = mongoose.model("Project", ProjectSchema);
+
+export default Project;
